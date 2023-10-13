91f4c1527deb47ffd01d133a43b9576c5fd7fbfb
Jorge Garcia <jorgegarcpon@gmail.com>
2023-10-13 00:06:54 -0400

    updates analog simulation script

--- Analog Landscape/pennylane_gaussian_perceptron_hessians.py
+++ Analog Landscape/pennylane_gaussian_perceptron_hessians.py
@@ -9,7 +9,7 @@ from time import time
 from datetime import datetime
 import pickle
 
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 
 import os, sys, argparse
@@ -30,11 +30,10 @@ parser.add_argument('--qubits', type=int, required=True)
 parser.add_argument('--field', type=float, required=True)
 parser.add_argument('--seed', type=int, required=True)
 parser.add_argument('-o', type=str, required=True)
-
 parser.add_argument('--basis', type=int, required=True)
 parser.add_argument('--sigma', type=float, required=True)
-parser.add_argument('--k', type=int, required=True)
-parser.add_argument('--n', type=int, required=True)
+# parser.add_argument('--k', type=int, required=True)
+# parser.add_argument('--n', type=int, required=True)
 parser.add_argument('-outdir', type=str, required=True)
 
 
@@ -61,18 +60,18 @@ def get_loss_function(perceptron, ts, H_obj, dev):
 
 
 # setting up the problem
-perceptron_qubits = argparse.qubits
-transverse_field_coefficient = argparse.field
-jax_seed = argparse.seed
-
-
-fourier_basis = 5
+perceptron_qubits = args.qubits
+transverse_field_coefficient = args.field
+jax_seed = args.seed
+n_pulse_basis = args.basis
+pulse_width = args.sigma
+out_file_path = args.o
 
 ts = jnp.array([0.0, 1.0])
 
 dev = qml.device("default.qubit.jax", wires = perceptron_qubits)
 
-perceptron = Perceptron(perceptron_qubits, fourier_basis, basis='gaussian')
+perceptron = Perceptron(perceptron_qubits, n_pulse_basis, basis='gaussian', pulse_width=0.01)
 
 H =  perceptron.H
 
@@ -86,8 +85,6 @@ print(f'Exact ground state energy: {e_ground_state_exact}')
 print('###########################################################################################\n')
 
 print(f'Starting simulation for {perceptron_qubits} qubit percetron with {transverse_field_coefficient} transverse field coefficient:')
-print(f'    Training information:\n        {100} epochs with learning rate of {1}')
-
 
 # getting the loss_function
 loss = get_loss_function(perceptron, ts, H_obj, dev)
@@ -106,20 +103,20 @@ print(f'Initial gradients: {initial_gradients}')
 value_and_grad = jax.jit(jax.value_and_grad(loss))
 
 
-n_epochs = 100
+n_epochs = 150
 param_vector = perceptron.get_random_parameter_vector(jax_seed)
 
 # The following block creates a constant schedule of the learning rate
 # that increases from 0.1 to 0.5 after 10 epochs
-# schedule0 = optax.constant_schedule(1e-1)
-# schedule1 = optax.constant_schedule(5e-1)
-# schedule = optax.join_schedules([schedule0, schedule1], [20])
-# optimizer = optax.adam(learning_rate=schedule)
-optimizer = optax.adam(learning_rate=1)
+schedule0 = optax.constant_schedule(1)
+schedule1 = optax.constant_schedule(0.1)
+schedule = optax.join_schedules([schedule0, schedule1], [30])
+optimizer = optax.adam(learning_rate=schedule)
+# optimizer = optax.adam(learning_rate=1)
 opt_state = optimizer.init(param_vector)
 
-energy = np.zeros(n_epochs + 1)
-energy[0] = loss(param_vector)
+energies = np.zeros(n_epochs )
+# energy[0] = loss(param_vector)
 mean_gradients = np.zeros(n_epochs)
 param_trajectory = []
 
@@ -128,17 +125,22 @@ time0 = time()
 _ = value_and_grad(param_vector)
 time1 = time()
 
-print(f"grad and val compilation time: {time1 - time0}")
+# print(f"grad and val compilation time: {time1 - time0}")
 
 
 ## Optimization loop
 for n in range(n_epochs):
     val, grads = value_and_grad(param_vector)
     updates, opt_state = optimizer.update(grads, opt_state)
-    param_vector = optax.apply_updates(param_vector, updates)
 
     mean_gradients[n] = np.mean(np.abs(grads))
-    energy[n+1] = val
+    energies[n] = val
+    param_trajectory.append(param_vector)
+
+    param_vector = optax.apply_updates(param_vector, updates)
+
+    # mean_gradients[n] = np.mean(np.abs(grads))
+    # energy[n+1] = val
 
     # print(f"            param: {param_vector}")
 
@@ -152,32 +154,55 @@ print('#########################################################################
 
 print('Results: ')
 
-print(f"    Found ground state: {energy[-1]}")
+print(f"    Found ground state: {energies[-1]}")
 
+# print(f' Test: {value_and_grad(param_trajectory[-1])}')
 
+# Calculating the Hessian at the final point
+
+final_hessian = jax.jacrev(jax.jacrev(loss))(param_trajectory[-1])
+final_hessian_eigenvalues = jnp.linalg.eigvals(final_hessian)
+print(final_hessian_eigenvalues)
+min_eigval = jnp.min(final_hessian_eigenvalues)
+max_eigval = jnp.max(final_hessian_eigenvalues)
+minmax_ratio = min_eigval/max_eigval
+
+print(f'Min/Max ratio: {minmax_ratio}')
 
 # Saving simulation results to dictionary
 simulation_results_dictionary = {
-    'parameters_trajectory': parameter_trajectory,
-    'cost_trajectory': cost_trajectory,
+    'parameters_trajectory': param_trajectory,
+    'energy_trajectory': energies,
+    'gradient_trajectory': mean_gradients
 }
 
 simulation_results_dictionary['final_hessian'] = {
-    'hessian_matrix': hessian_matrix,
-    'determinant': determinant,
-    'eigenvalues': eigenvalues,
-    'eigenvectors': eigenvectors,
+    'hessian_matrix': final_hessian,
+    'eigenvalues': final_hessian_eigenvalues,
     'min_eigval': min_eigval,
     'max_eigval': max_eigval,
     'minmax_ratio': minmax_ratio
 }
 
-
-
-
 data_dictionary = {
-      "qubits": perceptron_qubits,
-      "epochs": 100,
-      "transverse_field_coefficient": transverse_field_coefficient,
-      'learning_rate': 1,
+      'qubits': perceptron_qubits,
+      'epochs': 150,
+      'transverse_field_coefficient': transverse_field_coefficient,
+      'learning_rates': (1, 0.1),
+      'simulation_results': simulation_results_dictionary
 }
+
+
+import pickle
+
+# save dictionary to person_data.pkl file
+with open(out_file_path, 'wb') as fp:
+    pickle.dump(data_dictionary, fp)
+    print('dictionary saved successfully to file')
+
+
+with open(out_file_path, 'rb') as fp:
+    loaded_dict = pickle.load(fp)
+
+
+print(loaded_dict)
