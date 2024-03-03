639670603066516300035f69a2f872e5c44782e9
Jorge Garcia <jorgegarcpon@gmail.com>
2024-03-03 02:24:56 -0500

    fixed cluster_jobs_script

--- Quantum Natural Gradient/cluster_jobs_script.py
+++ Quantum Natural Gradient/cluster_jobs_script.py
@@ -17,12 +17,12 @@ HOURS = 6
 MINUTES = 0
 
 # Template for the filename
-filename_template = 'perceptron_${QUBITS}_qubits_${PULSES}_pulses_${LEARNING_RATE}_lr.sh'
+filename_template = 'perceptron_{qubits}_qubits_${basis}_pulses_${lr}_lr.sh'
 
 # Loop over each LEARNING_RATE value
 for lr in learning_rates:
     # Prepare the filename with the current LEARNING_RATE
-    filename = filename_template.format(qubits=QUBITS, pulses=BASIS, lr=lr)
+    filename = filename_template.format(qubits=QUBITS, basis=BASIS, lr=lr)
 
     # Prepare the bash command for envsubst
     bash_command = f'export QUBITS={QUBITS} BASIS={BASIS} LEARNING_RATE={lr} DAYS={DAYS} HOURS={HOURS} MINUTES={MINUTES} && envsubst \'$QUBITS, $BASIS, $LEARNING_RATE, $DAYS, $HOURS, $MINUTES\' < BATCH_TEMPLATE.sh > {filename}'
@@ -31,7 +31,7 @@ for lr in learning_rates:
     subprocess.run(bash_command, shell=True, check=True)
 
     # Now, run sbatch for the created file
-    sbatch_command = f'sbatch perceptron_{QUBITS}_qubits_{BASIS}_pulses_{lr}_lr.sh'
+    sbatch_command = f'sbatch {filename}'
     subprocess.run(sbatch_command, shell=True, check=True)
 
     print(f"Submitted batch job for {filename}")
